7adcb4fbb427bfb2f5d063ca89225faea1a28701
Luke Sherman <lwsherman123@gmail.com>
2023-03-27 12:07:05 +0100

    Changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { ArrowRightCircle } from 'react-bootstrap-icons'
 import { useState, useEffect } from 'react'
-import { HashLink } from 'react-router-hash-link'
 
 
 const Banner = () => {
